ea876c5682338213a7725eb190fe7707477b38a1
Pratham Nitin Dhanesha <dhanesha151001@gmail.com>
2024-01-31 16:35:39 +0530

    Update customer-who-visited-but-did-not-make-any-transactions.sql

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -5,3 +5,9 @@ LEFT JOIN Transactions T ON V.visit_id = T.visit_id
 WHERE T.visit_id IS NULL
 GROUP BY customer_id
 ORDER BY count_no_trans DESC;
+
+<!-- There is another way to solve the problem using NOT IN method; --> 
+SELECT customer_id, COUNT(*) as count_no_trans
+FROM Visits
+WHERE visit_id NOT IN (SELECT DISTINCT visit_id FROM Transactions)
+GROUP BY customer_id;
