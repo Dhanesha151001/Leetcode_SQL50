2ca80c016e969c66f5bb4385d9826a88e3034bc3
Pratham Nitin Dhanesha <dhanesha151001@gmail.com>
2024-11-10 15:42:48 +0530

    Time: 1015 ms (71.20%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,3 +1,6 @@
-select r.contest_id, round(count(distinct user_id) *100/ (select count(user_id)from users),2) as percentage from register r
+select r.contest_id, round(count(distinct user_id)*100/( select count(user_id) from users),2) as percentage from register r
+
 group by r.contest_id
-order by percentage desc,contest_id
+
+
+order by percentage DESC, r.contest_id
