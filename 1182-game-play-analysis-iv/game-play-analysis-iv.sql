e2eb180268b6306b5ee4ed6e2d6fd40e888b0ba7
Pratham Nitin Dhanesha <dhanesha151001@gmail.com>
2024-11-20 17:43:18 +0530

    Time: 679 ms (44.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,5 @@
+select round(sum(player_login_date)/count(distinct player_id),2) as fraction
+from
+(select player_id, 
+datediff (event_date, min(event_date) over (partition by player_id)) = 1 as player_login_date
+from activity) as new_table
