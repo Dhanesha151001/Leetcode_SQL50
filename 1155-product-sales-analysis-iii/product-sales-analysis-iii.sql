9321d636c7c524c905739c0afd9aad188f452be7
Pratham Nitin Dhanesha <dhanesha151001@gmail.com>
2024-11-21 19:10:05 +0530

    Time: 904 ms (98.12%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,3 +1,5 @@
 select s.product_id, (s.year) as first_year, quantity, price from Sales s
 where 
 ((product_id, year) IN (select product_id, min(year) from Sales group by product_id ) )
+
+#Used IN Clause because wanted to have multiple Where conditions
